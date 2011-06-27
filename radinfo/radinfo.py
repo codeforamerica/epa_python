dab226746aaadb5793796ec23e1705a9fd3551a7
Zach Williams <hey@zachwill.com>
2011-06-27 14:15:08 -0700

    Added correct catalog method

--- radinfo/radinfo.py
+++ radinfo/radinfo.py
@@ -4,6 +4,8 @@
 
 from api import API
 
+from lookup_table import lookup_table
+
 
 class RADInfo(API):
     """Python wrapper for the EPA's RADInfo API."""
@@ -12,9 +14,17 @@ class RADInfo(API):
         super(RADInfo, self).__init__()
         self.base_url = 'http://iaspub.epa.gov/enviro/efservice'
         self.output_format = 'xml'
+        self.lookup_table = lookup_table
 
-    def catalog(self, table):
-        pass
+    def catalog(self, table='', column=''):
+        """Lookup the values available for querying."""
+        lookup_table = self.lookup_table
+        if table:
+            if column:
+                return lookup_table[table][column]
+            return lookup_table[table]
+        # Show what tables are available.
+        return lookup_table.keys()
 
     def regulator_program(self, column=None, value=None, **kwargs):
         if not column and not value:

f3bad8a8d41c33ec2149f6c2d4dcdb663ab4ec26
Zach Williams <hey@zachwill.com>
2011-07-01 11:43:57 -0700

    Moved tests to a directory

---
+++ epa/tests/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from test_envirofacts import *
+from test_pcs import *
+from test_radinfo import *
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import unittest
+
+from epa.tests import *
+
+
+if __name__ == '__main__':
+    unittest.main()

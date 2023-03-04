bde99d44d667ec396708325183438029533e5774
Jason Leatherman <jwleath@yahoo.com>
2023-03-04 12:37:41 -0500

    Update to use two new lookup data types. Update api functions for new EPA requirements.

--- epa/envirofacts/envirofacts_api.py
+++ epa/envirofacts/envirofacts_api.py
@@ -3,15 +3,11 @@
 """
 All of the EPA's Envirofacts APIs follow the exact same structure -- a base
 URL, followed by a path that includes the table name, column name, and value.
-This module should be imported and built off of -- since it does has methods to
+This module should be imported and built off of -- since it has methods to
 handle calling the EPA in that way.
 """
 
-try:
-    from urllib import quote
-except ImportError:  # pragma: no cover
-    # For Python 3.
-    from urllib.parse import quote
+from urllib.parse import quote
 
 from .api import API, urlopen
 
@@ -21,48 +17,75 @@ class Envirofacts(API):
 
     def __init__(self):
         super(Envirofacts, self).__init__()
-        self.base_url = 'http://iaspub.epa.gov/enviro/efservice'
-        self.output_format = 'xml'
-        self.lookup_table = None
-        self.lookup_methods = None
+        self.base_url = 'https://data.epa.gov/efservice'
+        self.operations = ['=', '!=', '<', '>', 'BEGINNING', 'CONTAINING']
+        self.default_operation = '='
+        self.output_formats = ['XML', 'JSON', 'CSV', 'EXCEL']
+        self.default_output_format = 'xml'
+        self.table_lookup = None
+        self.table_column_lookup = None
+        self.column_table_lookup = None
 
     def catalog(self, table='', column=''):
         """Lookup the values available for querying."""
-        lookup_table = self.lookup_table
-        if lookup_table is not None:
-            if table:
-                if column:
-                    column = column.upper()
-                    return lookup_table[table][column]
-                return lookup_table[table]
-            # Show what methods are available.
-            return self.lookup_methods
+        if table:
+            if not self.table_lookup or not self.table_column_lookup:
+                return None
+            table = table.upper()
+            if table not in self.table_lookup.keys():
+                # raise ValueError("unknown table: {}".format(table))
+                return None
+        if column:
+            if not self.column_table_lookup:
+                return None
+            column = column.upper()
+            if column not in self.column_table_lookup.keys():
+                # raise ValueError("unknown column: {}".format(column))
+                return None
+        if table and column:
+            if column not in self.table_column_lookup[table].keys():
+                # raise ValueError(f"table {table} has no column {column}")
+                return None
+            return self.table_column_lookup[table][column]  # get column description
+        elif table:
+            return self.table_lookup[table]  # get table description
+        elif column:
+            return self.column_table_lookup[column]  # get list of tables that contain this column
         return None
 
-    def _resolve_call(self, table, column='', value='', **kwargs):
+    def _resolve_call(self, table_lst, column='', value='', **kwargs):
         """Internal method to resolve the API wrapper call."""
         if not column:
-            return self.catalog(table)
+            return self.catalog(table_lst[0])
         elif not value:
-            return self.catalog(table, column)
-        # We have all the table, column, and value, and now need to
+            return self.catalog(table_lst[0], column)
+        # We have the tables, column, and value. Now we need to
         # ensure they're all strings and uppercase.
         column = column.upper()
         value = str(value).upper()
-        data = self.call_api(table, column, value, **kwargs)
+        data = self.call_api(table_lst, column, value, **kwargs)
         if isinstance(data, dict):
             # Data is actually the first value.
             data = data.values()[0]
         return data
 
-    def call_api(self, table, column, value, **kwargs):
+    def call_api(self, table_lst, column, value, **kwargs):
         """Exposed method to connect and query the EPA's API."""
+        if type(table_lst) == str:
+            # A list of tables is expected, but handle a string
+            table_lst = [table_lst]
         try:
             output_format = kwargs.pop('output_format')
         except KeyError:
-            output_format = self.output_format
-        url_list = [self.base_url, table, column,
-                    quote(value), 'rows']
+            output_format = self.default_output_format
+        try:
+            op = kwargs.pop('operation').upper()
+        except KeyError:
+            op = self.default_operation
+        if op not in self.operations:
+            raise ValueError("illegal operation: {}".format(op))
+        url_list = [self.base_url, table_lst[0], column, op, quote(value)]
+        url_list.extend(table_lst[1:])
         rows_count = self._number_of_rows(**kwargs)
         url_list.append(rows_count)
         url_string = '/'.join(url_list)
@@ -70,9 +93,10 @@ class Envirofacts(API):
         data = self._format_data(output_format, xml_data)
         return data
 
-    def _number_of_rows(self, start=0, count=100, **kwargs):
+    @staticmethod
+    def _number_of_rows(start=0, count=100, **kwargs):
         """Internal method to format the number of rows the EPA API returns."""
         first = str(start)
         last = str(start + count)
-        string_format = ':'.join([first, last])
+        string_format = f"rows/{first}:{last}"
         return string_format
